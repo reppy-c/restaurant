8b9a1d166d13c6fff1a86a6425d2378e551da7e7
Jonathan Chiu <13129139+reppy-c@users.noreply.github.com>
2025-02-05 00:06:42 -0500

    add menu and about pages

--- src/about.js
+++ src/about.js
@@ -1,4 +1,37 @@
+import zeffImg from "./zeff.png";
+
+const image = document.createElement("img");
+const title = document.createElement("h3");
+const about = document.createElement("div");
+const aboutText = document.createElement("div");
+const description = document.createElement("p");
+
 export function renderAbout() {
+
     // Clear content div
     content.innerHTML = "";
+
+    // Create image element
+    image.src = zeffImg;
+
+    // Create title element
+    title.classList.add("about-title");
+    title.innerHTML = 'Welcome to Baratie – The Finest Floating Restaurant on the Grand Line!'
+
+    // Create description element
+    description.classList.add("about-description");
+    description.innerHTML = `Set sail for a dining experience like no other at Baratie, the legendary sea-bound restaurant known for its world-class cuisine and vibrant atmosphere! Founded by the renowned Head Chef Zeff, Baratie serves gourmet meals to pirates, marines, and travelers alike—no matter where their allegiances lie. From our famous seafood dishes to hearty stews that can satisfy even the fiercest of appetites, every plate is crafted with passion and precision. Dock your ship, enjoy breathtaking ocean views, and experience the hospitality of our skilled chefs and lively crew. Just remember—cause trouble, and you might find yourself on the wrong end of a flying kick!`;
+
+
+    aboutText.appendChild(title);
+    aboutText.appendChild(description);
+
+    // Add the elements to content
+    about.appendChild(image);
+    about.appendChild(aboutText);
+    about.classList.add("about");
+
+    content.appendChild(about);
+
+    console.log("render menu!");
 }
--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,81 @@
+// Get image assets
+
+import allbluestewImg from "./allbluestew.png";
+import fiestariceImg from "./fiestarice.png";
+import ramendelightImg from "./ramendelight.png";
+import berryImg from "./berry.png";
+
+// Define arrays of static content
+let menuItems = [
+    {   
+        name: "All Blue Stew", 
+        img: allbluestewImg, 
+        description: "Sea King meat chunks, Grand Line cabbage, orange carrots from Cocoyashi Village, East Blue potatoes, and a savory spice-rich broth with a dash of Calm Belt sea salt.",
+        blurb: "A hearty stew inspired by the mythical All-Blue, blending the unique flavors of every sea into one unforgettable dish. A must-try for any pirate dreaming of culinary treasure!",
+        price: "1,800 berry"
+    },
+    { 
+        name: "Baratie's Seafood Fiesta Rice", 
+        img: fiestariceImg, 
+        description: "Golden saffron rice, South Blue shrimp, Calm Belt squid rings, shellfish from the All-Blue, Grand Line bell peppers, and a garnish of citrus zest from Sniper Island lemons." ,
+        blurb: "A vibrant rice dish crafted with seafood straight from the Grand Line, capturing the adventurous essence of Baratie’s floating restaurant. Every bite feels like a journey through the seas.",
+        price: "4,300 berry"
+    },
+    { 
+        name: "East Blue Ramen Delight", 
+        img: ramendelightImg, 
+        description: "Ramen noodles, broth made from East Blue seaweed and fish, fish cake shaped like the Jolly Roger, steamed asparagus spears, and a drizzle of syrup from Syrup Village.",
+        blurb: "A light yet flavorful ramen soup inspired by the peaceful yet daring waters of East Blue. Perfect for recharging before setting sail on new adventures.",
+        price: "2,500 berry"
+    }
+];
+
+// Define variables
+const content = document.querySelector('#content');
+const berry = document.createElement("img");
+berry.src = berryImg;
+
 export function renderMenu() {
+
     // Clear content div
     content.innerHTML = "";
+    const menu = document.createElement("div");
+
+    // Loop through the menu items and create elements for each
+    menuItems.forEach(element => {
+        const menuItem = document.createElement("div");
+        const menuItemImg = document.createElement("img");
+        const menuItemText = document.createElement("div");
+        const menuItemTextTitle = document.createElement("h3");
+        const menuItemTextDesc = document.createElement("p");
+        const menuItemTextBlurb = document.createElement("p");
+        const menuItemTextPrice = document.createElement("span");
+        
+        menu.classList.add("menu");
+        menuItem.classList.add("menu-item");
+        menuItemText.classList.add("menu-text");
+        menuItemTextTitle.classList.add("menu-title");
+        menuItemTextDesc.classList.add("menu-desc");
+        menuItemTextBlurb.classList.add("menu-blurb");
+        menuItemTextPrice.classList.add("menu-price");
+
+        menuItemImg.src=element.img;
+        menuItemTextTitle.innerHTML = element.name;
+        menuItemTextDesc.innerHTML = element.description;
+        menuItemTextBlurb.innerHTML = element.blurb;
+        menuItemTextPrice.innerHTML = element.price;
+
+        menuItem.appendChild(menuItemImg);
+        menuItem.appendChild(menuItemText);
+
+        menuItemText.appendChild(menuItemTextTitle);
+        menuItemText.appendChild(menuItemTextDesc);
+        menuItemText.appendChild(menuItemTextBlurb);
+        menuItemText.appendChild(menuItemTextPrice);
+
+        menu.appendChild(menuItem);    
+    });
+
+    content.appendChild(menu);
+    console.log("render menu!");
 }
